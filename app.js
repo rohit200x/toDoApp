c91c3111da26e4dd844fffde2e9f8ab33e4d808c
rohit200x <rohitshendurkar2017@gmail.com>
2024-07-18 16:46:04 +0530

    fixed cors in app.js

--- app.js
+++ app.js
@@ -1,30 +1,35 @@
-import express from "express"
-import userRouter from "./routes/user.js"
-import taskRouter from "./routes/task.js"
-import {config} from "dotenv"
-import cookieParser from "cookie-parser"
-import { errorMiddleware } from "./middlewares/error.js"
-import cors from "cors"
+import express from "express";
+import userRouter from "./routes/user.js";
+import taskRouter from "./routes/task.js";
+import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import { errorMiddleware } from "./middlewares/error.js";
+import cors from "cors";
+
+export const app = express();
 
 config({
-    path:"./data/config.env"
-})
-
-export const app=express({
-    origin:[process.env.FRONTEND_URL],
-    methods:["GET","POST","PUT","DELETE"],
-    credentials:true    
+  path: "./data/config.env",
 });
 
+// Using Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
-app.use("/api/v1/users",userRouter);
-app.use("/api/v1/task",taskRouter);
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
-app.get("/",(req,res)=>{
-    res.send("nice working");
-})
+// Using routes
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/task", taskRouter);
 
+app.get("/", (req, res) => {
+  res.send("Nice working");
+});
 
+// Using Error Middleware
 app.use(errorMiddleware);
